f7eaa7c4b978719a10f2acec19156370f035a000
JessickaMoya <jessicka.moya@gmail.com>
2021-07-13 18:37:43 -0400

    chapter 4.1 and 4.2

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -1,39 +1,36 @@
 package base;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pages.HomePage;
 
 import java.util.List;
 
 public class BaseTests {
 
     private WebDriver driver;
+    protected HomePage homePage;
 
+    @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get("https://the-internet.herokuapp.com/");
 
-        List<WebElement> links = driver.findElements(By.tagName("a"));
-        System.out.println(links.size());
-
-        WebElement inputsLink = driver.findElement(By.linkText("Inputs"));
-        inputsLink.click();
+        homePage = new HomePage(driver);
 
         //Optional Exercise Chapter 3
 //        optionalExerciseChapter3(driver);
         //Optional Exercise Chapter 3
-        
-        System.out.println(driver.getTitle());
-        driver.quit();
     }
 
-    public static void main(String[] args) {
-        BaseTests test = new BaseTests();
-        test.setUp();
+    @AfterClass
+    public void tearDown() {
+        driver.quit();
     }
 
     public static void optionalExerciseChapter3(WebDriver driver) {
