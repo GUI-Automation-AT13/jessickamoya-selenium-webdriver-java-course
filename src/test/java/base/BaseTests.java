523c8cc7c244b9ce61d7e7e3b344f41fdc6efafd
JessickaMoya <jessicka.moya@gmail.com>
2021-07-14 18:16:04 -0400

    chapter 14 and optional exercise

--- src/test/java/base/BaseTests.java
+++ src/test/java/base/BaseTests.java
@@ -3,6 +3,7 @@ package base;
 import com.google.common.io.Files;
 import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.events.EventFiringWebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterClass;
@@ -10,6 +11,7 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import pages.HomePage;
+import utils.CookieManager;
 import utils.EventReporter;
 import utils.WindowManager;
 import java.io.File;
@@ -25,18 +27,18 @@ public class BaseTests {
     @BeforeClass
     public void setUp() {
         System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
-        driver = new EventFiringWebDriver(new ChromeDriver());
+        driver = new EventFiringWebDriver(new ChromeDriver(getChromeOptions()));
         driver.register(new EventReporter());
 //        driver = new ChromeDriver();
 //        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         goHome();
-
-        homePage = new HomePage(driver);
+//        setCookie();
     }
 
     @BeforeMethod
     public void goHome() {
         driver.get("https://the-internet.herokuapp.com/");
+        homePage = new HomePage(driver);
     }
 
     @AfterClass
@@ -73,6 +75,23 @@ public class BaseTests {
         return new WindowManager(driver);
     }
 
+    private ChromeOptions getChromeOptions() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("disable-infobars");
+//        options.setHeadless(true);
+        return options;
+    }
+
+    private void setCookie() {
+        Cookie cookie = new Cookie.Builder("tau", "123")
+                .domain("the-internet.herokuapp.com")
+                .build();
+        driver.manage().addCookie(cookie);
+    }
+
+    public CookieManager getCookieManager(){
+        return new CookieManager(driver);
+    }
 
     //Optional Exercise Chapter 3
     public static void optionalExerciseChapter3(WebDriver driver) {
