1a936fe887393657f50d69e3f17ceaf955e199fa
JessickaMoya <jessicka.moya@gmail.com>
2021-07-14 00:52:53 -0400

    chapter 10 and optional exercise

---
+++ src/test/java/javascript/JavaScriptTests.java
@@ -0,0 +1,30 @@
+package javascript;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+import java.util.List;
+import static org.testng.Assert.*;
+
+public class JavaScriptTests extends BaseTests {
+
+    @Test
+    public void testScrollToTable(){
+        homePage.clickLargeAndDeepDom().scrollToTable();
+    }
+
+    @Test
+    public void testScrollToFifthParagraph(){
+        homePage.clickInfiniteScroll().scrollToParagraph(5);
+    }
+
+    @Test
+    public void testAddMultipleAttributesToDropdown() {
+        var dropDownPage = homePage.clickDropDown();
+        dropDownPage.addMultipleAttributes();
+        var optionsToSelect = List.of("Option 1", "Option 2");
+        optionsToSelect.forEach(dropDownPage::selectFromDropDown);
+        var selectedOptions = dropDownPage.getSelectedOptions();
+        assertTrue(selectedOptions.containsAll(optionsToSelect), "All options were not selected");
+        assertEquals(selectedOptions.size(), optionsToSelect.size(), "Number of items that were selected");
+    }
+}
